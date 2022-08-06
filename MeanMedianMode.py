e419f41dd5b87f9276188726a4164dea3a297bf6
THE-UNKNOWN-CREATOR <70652146+THE-UNKNOWN-CREATOR@users.noreply.github.com>
2022-08-06 17:37:56 +0530

    Update MeanMedianMode.py

--- MeanMedianMode.py
+++ MeanMedianMode.py
@@ -34,14 +34,25 @@ def median(vals) :
         return median
 
 def mode(vals):
-    floored_vals = []
+    data = Counter(vals)
+    mode_data_for_range = {
+        "50-60": 0,
+        "60-70": 0,
+        "70-80": 0
+    }
+    for height, occurence in data.items():
+        if 50 < float(height) < 60:
+            mode_data_for_range["50-60"] += occurence
+        elif 60 < float(height) < 70:
+            mode_data_for_range["60-70"] += occurence
+        elif 70 < float(height) < 80:
+            mode_data_for_range["70-80"] += occurence
 
-    for v in vals:
-        f_v = v//1
-        floored_vals.append(f_v)
-
-    freq = Counter(floored_vals)
-    mode = freq.most_common(1)[0][0]
+    mode_range, mode_occurence = 0, 0
+    for range, occurence in mode_data_for_range.items():
+        if occurence > mode_occurence:
+            mode_range, mode_occurence = [int(range.split("-")[0]), int(range.split("-")[1])], occurence
+    mode = float((mode_range[0] + mode_range[1]) / 2)
     return mode
 
 
